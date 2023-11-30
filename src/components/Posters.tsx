ca82c199f6da0203968f7f0dd148cf8f0267832b
Zach Lefler <zachary.lefler@gmail.com>
2023-11-30 11:44:29 -0800

    switch drag drop libraries

--- src/components/Posters.tsx
+++ src/components/Posters.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, Button } from '@mui/material'
-import dragula from 'react-dragula'
-import { useEffect, useRef } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+// import { useEffect, useRef } from 'react'
 
 const Posters = ({
   movieData,
@@ -12,49 +12,23 @@ const Posters = ({
   ratingsMode,
   onReset,
 }) => {
-  const movieContainerRef = useRef(null)
-
-  useEffect(() => {
-    if (movieContainerRef.current && movieData.movies.length) {
-      const drake = dragula([movieContainerRef.current], {})
-
-      drake.on('drop', function (el, target, source, sibling) {
-        const draggedTitle = el.querySelector('p').innerText
-
-        const newInputValues = [...inputValues]
-
-        const oldIndex = newInputValues.indexOf(draggedTitle)
-
-        let newIndex
-        if (!sibling) {
-          // If there's no sibling, the movie was dragged to the end
-          newIndex = newInputValues.length - 1
-        } else {
-          // If there's a sibling, find the index of the sibling in the array
-          const siblingTitle = sibling.querySelector('p').innerText
-          newIndex = newInputValues.indexOf(siblingTitle)
-
-          // If dragging down, insert before the sibling's index; if up, after
-          if (oldIndex < newIndex) {
-            newIndex--
-          }
-        }
-        // Remove the dragged item from its old position
-        newInputValues.splice(oldIndex, 1)
-
-        // Insert the dragged item at its new position
-        newInputValues.splice(newIndex, 0, draggedTitle)
-
-        // Update the state with the new order
-        setInputValues(newInputValues)
-      })
-
-      return () => drake.destroy()
-    }
-  }, [movieData?.movies, inputValues, setInputValues])
-
   const getColor = (title, index) => {
     return inputValues.indexOf(title) === index ? 'green' : 'red'
+  }
+
+  const reorder = (startIndex, endIndex) => {
+    const result = Array.from(inputValues)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return
+    }
+    const items = reorder(result.source.index, result.destination.index)
+    setInputValues(items)
   }
 
   return (
@@ -74,50 +48,69 @@ const Posters = ({
           <p style={{ marginLeft: '2%' }}>highest</p>
           <p style={{ marginRight: '2%' }}>lowest</p>
         </div>
-        <div
-          ref={movieContainerRef}
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            flexWrap: 'wrap',
-          }}
-        >
-          {movieData.movies.map((movie, index) => (
-            <div
-              key={movie.title}
-              style={{
-                width: '18%',
-                margin: '1%',
-                textAlign: 'center',
-              }}
-            >
-              {submitted && <p>{inputValues.indexOf(movie.title) + 1}</p>}
-              <Card>
-                <img
-                  alt={movie.title}
-                  src={movie.poster}
-                  style={{ maxHeight: 300, maxWidth: '100%' }}
-                />
-                <p
-                  style={{
-                    marginTop: '0.5rem',
-                  }}
-                >
-                  {movie.title}
-                </p>
-              </Card>
-              {submitted && (
-                <p style={{ color: getColor(movie.title, index) }}>
-                  {ratingsMode ? movie.rating : movie.boxOffice}
-                </p>
-              )}
-            </div>
-          ))}
-        </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable-movie-list" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  flexWrap: 'wrap',
+                  // background: snapshot.isDraggingOver ? 'lightblue' : 'inherit', // Optional: change background color when dragging over
+                }}
+                {...provided.droppableProps}
+              >
+                {inputValues.map((title, index) => {
+                  const movie = movieData.movies.find((m) => m.title === title)
+                  return (
+                    <Draggable
+                      key={movie.title}
+                      draggableId={movie.title}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            ...provided.draggableProps.style,
+                            width: '18%',
+                            margin: '1%',
+                            textAlign: 'center',
+                          }}
+                        >
+                          {submitted && (
+                            <p>{inputValues.indexOf(movie.title) + 1}</p>
+                          )}
+                          <Card>
+                            <img
+                              alt={movie.title}
+                              src={movie.poster}
+                              style={{ maxHeight: 300, maxWidth: '100%' }}
+                            />
+                            <p style={{ marginTop: '0.5rem' }}>{movie.title}</p>
+                          </Card>
+                          {submitted && (
+                            <p style={{ color: getColor(movie.title, index) }}>
+                              {ratingsMode ? movie.rating : movie.boxOffice}
+                            </p>
+                          )}
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
         <Button
           sx={{ width: '300px', marginTop: '30px' }}
           variant="contained"
-          onClick={() => handleSubmit()}
+          onClick={handleSubmit}
         >
           Submit
         </Button>
